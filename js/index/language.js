a5828a268c71f8ab661190cda9e73251cde9b6ce
Angular1337 <gtmrvsmcpd@gmail.com>
2025-05-13 23:52:53 +0300

    Update language.js

--- js/index/language.js
+++ js/index/language.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('usa').onclick = function() {
-        window.location.href = '/../../index_eng.html'; // Замените на нужный URL
+        window.location.href = 'https://github.com/Angular1337/Angular-Remix/index_eng.html'; // Замените на нужный URL
     };
 });
 document.addEventListener('DOMContentLoaded', function() {
