736b3c3e8f37488e0624909112e1e42a637123ad
Angular1337 <gtmrvsmcpd@gmail.com>
2025-05-19 16:05:30 +0300

    Update language.js

--- js/index/language.js
+++ js/index/language.js
@@ -1,10 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('usa').onclick = function() {
-        window.location.href = '/Angular-Remix/index_eng.html'; // Замените на нужный URL
+        window.location.href = './index_eng.html'; // Замените на нужный URL
     };
 });
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('russia').onclick = function() {
-        window.location.href = '/../../index.html'; // Замените на нужный URL
+        window.location.href = './index.html'; // Замените на нужный URL
     };
 });
